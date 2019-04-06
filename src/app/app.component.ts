d2450df3e46d29439a3fcc1b01b38d28715811be
Anna Anks Nowak <matisnape@wp.pl>
2019-04-06 12:53:44 +0200

    Use ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,26 @@ import { Component } from '@angular/core';
     <h1>
       Welcome to {{ title }}!
     </h1>
+
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-list';
+  title = 'app';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }

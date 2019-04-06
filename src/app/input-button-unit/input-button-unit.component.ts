90621376f5a9eb5c860484b0d44b43c28ff3fb59
Anna Anks Nowak <matisnape@wp.pl>
2019-04-06 11:39:07 +0200

    Add Input Button Unit component with properties

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+  `,
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+
+  constructor() {
+    this.title = 'I Love Angular';
+  }
+
+  ngOnInit() {
+    this.title = 'Angular CLI Rules!';
+  }
+
+}

4b2ade5eef1a69d5186c00a9cff51c9d44e1cd76
Anna Anks Nowak <matisnape@wp.pl>
2019-04-06 15:11:30 +0200

    Add styling

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,15 +3,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
-    <input #inputElementRef
+    <input class="todo-input"
+        #inputElementRef
         [value]="title"
         (keyup.enter)="submitValue($event.target.value)">
 
-    <button (click)="submitValue(inputElementRef.value)">
+    <button class="btn"
+            (click)="submitValue(inputElementRef.value)">
       Save
     </button>
   `,
